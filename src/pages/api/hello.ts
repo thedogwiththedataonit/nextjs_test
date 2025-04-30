adaadee68d38261ed4379d15fc6a2eec0dc2fb93
njerschow <nj18@nyu.edu>
2025-04-30 15:07:23 -0400

    update route

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,6 +9,6 @@ export default async function handler(
     res.status(200).json({ message: "This will never execute" });
   } else {
     res.setHeader("Allow", ["GET", "POST"]);
-    res.status(405).json({ message: `Method ${req.method} Not Allowed` });
+    res.status(500).json({ message: `Method ${req.method} Not Allowed` });
   }
 }
