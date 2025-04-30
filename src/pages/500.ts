32f9e3572ddc896805cc8d8df347b2d9f6d4209e
njerschow <nj18@nyu.edu>
2025-04-30 15:33:00 -0400

    add 500

--- src/pages/500.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest } from "next";
-import { NextApiResponse } from "next";
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(500).json({ message: "Internal Server Error" });
-}
