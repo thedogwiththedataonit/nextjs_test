d1137405349589d2fbb1c43c43e1759b75451321
Thomas Park <thomas0806.park@gmail.com>
2025-05-12 12:21:37 -0700

    fix

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -4,11 +4,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  res.setHeader("Allow", ["GET", "POST"]);
+  
   if (["GET", "POST"].includes(req.method || "")) {
     throw new Error("please give me a 500 error here");
     res.status(200).json({ message: "This will never execute" });
   } else {
-    res.setHeader("Allow", ["GET", "POST"]);
-    res.status(500).json({ message: `Method ${req.method} Not Allowed` });
+    res.status(405).json({ message: `Method ${req.method} Not Allowed` });
   }
 }
